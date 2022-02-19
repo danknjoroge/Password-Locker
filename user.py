58a641bf15f5c4e8a804f95d3e0a69c3eceb795c
danknjoroge <dank8505@gmail.com>
2022-02-19 08:16:43 +0300

    instantiated user

--- user.py
+++ user.py
@@ -1,9 +1,13 @@
 class User:
     '''
-    
+    A user class that will be used to generate class instances user
     '''
     def __init__(self, username, password):
-        pass
+        '''
+         __init__ method to define properties of user object
+        '''
+        self.username = username
+        self.password = password
 
 
 
