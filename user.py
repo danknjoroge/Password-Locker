8120dc4defb5ef78fe0d65c3a0293b7023cdb142
danknjoroge <dank8505@gmail.com>
2022-02-19 08:24:42 +0300

    created save user method

--- user.py
+++ user.py
@@ -2,12 +2,22 @@ class User:
     '''
     A user class that will be used to generate class instances user
     '''
+
+    user_list = []
+
     def __init__(self, username, password):
         '''
          __init__ method to define properties of user object
         '''
         self.username = username
         self.password = password
+
+
+    def save_user(self):
+        User.user_list.append(self)
+
+
+    
 
 
 
