f5230e3919d1fc7d997f5f22ee6b8d8196277073
danknjoroge <dank8505@gmail.com>
2022-02-20 13:13:32 +0300

    added login function in main

--- main.py
+++ main.py
@@ -51,13 +51,14 @@ def main():
     print(f'Hello {name}, enjoy as you explore our applications!')
 
     while True:
-        print("Use the following shortcodes cu: create user account \n log: login to your account \n dis: display names of the user \n ex: exit from the account")
-        shortcodes = input().lower
-
-        if shortcodes == 'cu' :
+        print("Use the following codes \n cr: create user account \n log: login to your account \n dis: display names of the user \n ex: exit from the account")
+        code = input().lower()
+        
+        if code == 'cr':
             '''creating user account'''
+
             print("New User Account")
-            print('_'*11)
+            print("-" *10)
 
             print('Input Username')
             username = input()
@@ -67,6 +68,46 @@ def main():
 
             save_users(create_users(username, password))
             print(f'Hi {username} account Successfully created')
+
+        elif code == 'log':
+            print('Enter you information to login into your account')
+            print('Please enter your username')
+            username = input()
+            print('Enter your password')
+            password = input()
+
+            if login_user(username, password) == None:
+                print('If you have an account try again, if not create an account')
+            else:
+                login_user(username, password)
+                print(f'Welcome {username} Select the service you want below')
+                while True:
+                    print('Select shortcode related to servce you want \n ca: add a credential \n dc: display credentials \n gc: generate password for account \n ex: back to main menu')
+                    code = input().lower()
+                    if code == 'ca':
+                        print('Create a new Account Credential')
+                        print('-'*10)
+
+                        print('Input account name:')
+                        accountName = input()
+
+                        print('Input account Username:')
+                        username = input()
+
+                        print('Input account Password:')
+                        password = input()
+
+                        save_credentials(create_credentials(accountName, username, password))
+                        print(f'Successfully created credentials for {accountName} account')
+
+                    elif code == 'dc':
+                        if display_credentials(accountName):
+                            print(f'{accountName} credentials')
+                            
+                        
+
+
+
         
 
 
